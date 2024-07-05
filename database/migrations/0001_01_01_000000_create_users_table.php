fd8ddb17d0bba31f6a768eb689d409ea7974d8ed
Alisa <alisamargaryan12@gmail.com>
2024-07-06 00:20:27 +0400

    push main files

---
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('name');
+            $table->string('lastname');
+            $table->string('email')->unique();
+            $table->string('role')->comment('admin => 1,  user => 2');
+            $table->string('phone_number')->nullable();
+            $table->string('password');
+            $table->string('profile_attachment')->nullable();
+            $table->longText('address')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};

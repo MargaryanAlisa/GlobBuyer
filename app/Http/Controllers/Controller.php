7c164658060be8b5017bb3a62cae9be37efa84a1
MargaryanAlisa <alisamargaryan12@gmail.com>
2024-07-31 00:58:56 +0400

    upload swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,22 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ * title="Swagger with Laravel",
+ * version="1.0.0",
+ * )
+ * @OA\PathItem(
+ *     path="/",
+ * )
+ * @OA\SecurityScheme(
+ * type="http",
+ * securityScheme="bearerAuth",
+ * scheme="bearer",
+ * bearerFormat="JWT"
+ * )
+ */
+
 abstract class Controller
 {
     //
